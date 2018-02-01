c7a953f49ad95859a580f2dd7ee1780bcd26e8c2
tushar khurana <tusharkhurana99@gmail.com>
2018-02-01 21:51:30 +0530

    Add new feature to add new topic in a discussion board (#3)
    * Add new feature to add new topic in a discussion board using these:
        * Add django form for new topic
        * Add webpage to list topics under a board
        * Add bootstrap to django form using django-tweaks
        * Add reusable form template
    
    * Fix Codacy issues
    
    * Modify travis.yml for python3.7 allowed failure and improve Codacy issues

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![Build Status](https://travis-ci.org/tkhurana96/Django-boards.svg?branch=master)](https://travis-ci.org/tkhurana96/Django-boards)
 [![codecov](https://codecov.io/gh/tkhurana96/Django-boards/branch/master/graph/badge.svg)](https://codecov.io/gh/tkhurana96/Django-boards)
+[![Code Health](https://landscape.io/github/tkhurana96/Django-boards/master/landscape.svg?style=flat)](https://landscape.io/github/tkhurana96/Django-boards/master)
+
 
 Discussion forum webapp in python using django.
 
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Board
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Board, Post
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 # Create your views here.
 
@@ -13,3 +14,30 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+
+    if request.method == 'POST':
+
+        form = NewTopicForm(request.POST)
+
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to created topic page
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
