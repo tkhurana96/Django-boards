bf5b45f0115dbf37777848e97e8ec830f83ab549
tushar khurana <tusharkhurana99@gmail.com>
2018-02-02 15:04:50 +0530

    Add the signup feature

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 
+from accounts import views as account_views
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', account_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls)
