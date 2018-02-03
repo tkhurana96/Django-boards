c5c80537434564a0a463a0dbd4aa6180ed6b7097
tushar khurana <tusharkhurana99@gmail.com>
2018-02-03 23:46:55 +0530

    Add the logout feature

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
 from accounts import views as account_views
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', account_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls)
