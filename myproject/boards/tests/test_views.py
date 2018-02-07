c8df7882d5bf8a2ca3528f84ecad735539eb1cfa
tushar khurana <tusharkhurana99@gmail.com>
2018-02-08 00:07:31 +0530

    Add template form-tags and their tests

--- myproject/boards/tests/test_views.py
+++ myproject/boards/tests/test_views.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.urls import resolve
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
 from django.contrib.auth.models import User
-from .forms import NewTopicForm
+from ..forms import NewTopicForm
 # Create your tests here.
 
 
